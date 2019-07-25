a261abb4302cfbcb228b5af202fb17523aaf815d
RyanPPitts <47765262+RyanPPitts@users.noreply.github.com>
2019-07-24 20:19:39 -0700

    Adding quantity field to the schema

---
+++ models/item.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose'); // require mongoose
+
+const Schema = mongoose.Schema; //Schema Constructor
+
+
+
+const itemSchema = new Schema({
+    type: String, //console, equipment or game
+    platform: String, //what platform your item is used for
+    title: String, //item name
+    quantity: Number, // how many you want to sell
+    condition: String, //new, used, damaged, for parts ect
+    description: String, //description on wear
+}, { timestamps: true })
+
+const Item = mongoose.model('Item', itemSchema);
+
+//Exported for access in `app.js`
+module.exports = Item;
