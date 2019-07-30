a442c52c41d9fe9a3070f15efc9f86ccec91495f
travis scott <ellevis54@yahoo.com>
2019-07-30 00:04:52 -0700

    added partial login logic

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.get("/new", (req, res) => {
+    res.render("users/new.ejs");
+})
+
+router.get("/login", (req, res) => {
+    res.render("user/login.ejs");
+})
+
+router.post("/", async(req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        console.log(newUser);
+        res.send(newUser);
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+router.post("/login", async(req, res) => {
+    console.log(req.body);
+    res.send("trying to login");
+})
+
 console.log('this is the user login controller');
 
 // youtube video talking about userController - https://www.youtube.com/watch?time_continue=611&v=v66XMIzwjSM
