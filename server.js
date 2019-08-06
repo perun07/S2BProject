4ea90dcba55571abe04ef57d04175b4bbfaf63eb
P Lowe <perun@reagan.com>
2019-08-06 03:30:54 -0400

    heroku working

--- server.js
+++ server.js
@@ -36,8 +36,10 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
 
+const uri = "mongodb://perun07:UpqIbVgOkl0cajHm@cluster0-shard-00-00-qjm1o.mongodb.net:27017,cluster0-shard-00-01-qjm1o.mongodb.net:27017,cluster0-shard-00-02-qjm1o.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 // mongoose.connect('mongodb://perun07:UpqIbVgOkl0cajHm@cluster0-shard-00-00-qjm1o.mongodb.net:27017,cluster0-shard-00-01-qjm1o.mongodb.net:27017,cluster0-shard-00-02-qjm1o.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true });
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(uri, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to mongo');
 })
@@ -53,7 +55,7 @@ app.use('/homepage', express.static('homepage'));
 
 // var MongoClient = require('mongodb').MongoClient;
 
-// var uri = "mongodb://perun07:Yellow198435@cluster0-shard-00-00-qjm1o.mongodb.net:27017,cluster0-shard-00-01-qjm1o.mongodb.net:27017,cluster0-shard-00-02-qjm1o.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 // MongoClient.connect(uri, function(err, client) {
 //   const collection = client.db("test").collection("devices");
 //   // perform actions on the collection object
