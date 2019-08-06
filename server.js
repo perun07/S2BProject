030c5e114e5f5d308e543d27d3abc7a406fdfc19
P Lowe <perun@reagan.com>
2019-08-06 15:40:50 -0400

    updating my origin to new heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 //Dependencies
 const express = require('express');
 const app = express();
+//parse
 app.use(express.urlencoded({ extended: true }))
 
 
@@ -16,18 +17,18 @@ const connectionString = process.env.MONGODB_URI;
 //Mongoose
 const mongoose = require('mongoose');
 
-//Method Override
+//Secret
 const session = require('express-session')
-app.use(session({
-    secret: "beepboopbeepbeepboop",
-    resave: false,
-    saveUninitialized: false
-}))
+
+//Method Override
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+
+//controllers
 const userController = require('./controller/userController');
 const itemController = require('./controller/itemController');
 const adminController = require('./controller/adminController');
+
 // this is the route that gets us to the account page
 app.use('/account', itemController);
 
@@ -36,6 +37,8 @@ app.use('/account', itemController);
 app.use('/public', express.static('public'));
 app.use('/users', userController);
 app.use('/admin', adminController);
+
+//setting port and mongo
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI
 
