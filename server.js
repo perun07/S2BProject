d69b13479383d9c935d4efef7b649f1ad97ff133
P Lowe <perun@reagan.com>
2019-08-06 00:41:58 -0400

    fixing

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/public', express.static('public'));
 
 app.use('/users', userController);
 app.use('/admin', adminController);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGODB_URI = 'mongodb://localhost/' + 'items';
 
 app.listen(PORT, () => {
