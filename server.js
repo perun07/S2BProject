d15614dc168ca2749e9996e9095426662e3f13c9
P Lowe <perun@reagan.com>
2019-08-06 22:11:01 -0400

    updating the master by including localhost code

--- server.js
+++ server.js
@@ -63,3 +63,52 @@ app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
 
+
+//local working code//keep//
+// console.log("this is the server.js file");
+
+// //Dependencies
+// const express = require('express');
+// const app = express();
+// app.use(express.urlencoded({ extended: true }))
+
+// //Mongoose
+// const mongoose = require('mongoose');
+
+// //Method Override
+// const session = require('express-session')
+// app.use(session({
+//     secret: "beepboopbeepbeepboop",
+//     resave: false,
+//     saveUninitialized: false
+// }))
+// const methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
+// const userController = require('./controller/userController');
+// const itemController = require('./controller/itemController');
+// const adminController = require('./controller/adminController');
+// // this is the route that gets us to the account page
+// app.use('/account', itemController);
+
+
+// // css file
+// app.use('/public', express.static('public'));
+
+// app.use('/users', userController);
+// app.use('/admin', adminController);
+// const PORT = 3000;
+// const MONGODB_URI = 'mongodb://localhost/' + 'items';
+
+// app.listen(PORT, () => {
+//     console.log('listening on port', PORT);
+// });
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongo');
+// })
+
+// //linking other pages//
+// app.set('view engine', 'ejs');
+// app.set('views');
+// app.use('/homepage', express.static('homepage'));
